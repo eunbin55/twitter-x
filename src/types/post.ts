affb764ead8de3677fa95143985e360aa32394dc
eunbin55 <eunbin620@naver.com>
2025-09-19 21:01:56 +0900

    feat: types(post, user)

---
+++ src/types/post.ts
@@ -0,0 +1,14 @@
+import { User } from "./user";
+
+export interface Post {
+  id: number;
+  author: User;
+  content: string;
+  images?: string[];
+  createdAt: string;
+  likes: number;
+  retweets: number;
+  comments: number;
+  isLiked: boolean;
+  isRetweeted: boolean;
+}
